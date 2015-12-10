881905a5a3029c8a32c331fcdb4922aaf8487357
bhoward <bhoward@lexmachina.com>
2015-12-10 08:01:56 +0000

    rewrite and move from my other project

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = [
+    'rpi.gpio',
+    'spidev',
+    ]
+
+setup(name='mrfc522',
+      version='0.1',
+      description='wrapper for RPi',
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      author='bchoward',
+      author_email='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      entry_points="""\
+      """,
+      )
+
